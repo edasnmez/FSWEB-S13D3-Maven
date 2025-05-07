c06aac94cc20b259cea3465d501f6b533a6b051b
edasnmez <eda_sonmez_52@hotmail.com>
2025-05-07 18:26:17 +0300

    Project completed

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,26 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world");
+        Person person = new Person("John", "Doe", 20);
+
+        System.out.println("Firstname: " + person.getFirstName());
+        System.out.println("LastName: " + person.getLastName());
+        System.out.println("Age: " + person.getAge());
+        System.out.println("Is Teen: " + person.isTeen());
+
+        Person person2 = new Person("Jane", "Doe", 17, "Female", "New York", "Engineer");
+
+        System.out.println("Firstname: " + person2.getFirstName());
+        System.out.println("LastName: " + person2.getLastName());
+        System.out.println("Age: " + person2.getAge());
+        System.out.println("Is Teen: " + person2.isTeen());
+
+
+        Wall wall = new Wall(5,4);
+        System.out.println("area= " + wall.getArea());
+        wall.setHeight(-1.5);
+        System.out.println("width= " + wall.getWidth());
+        System.out.println("height= " + wall.getHeight());
+        System.out.println("area= " + wall.getArea());
     }
 }
